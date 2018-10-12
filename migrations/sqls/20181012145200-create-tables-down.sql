84d0389ce165aae3b75bf215856ff4b71c72b496
ilya-kuznetsov <et.anchoret@gmail.com>
2018-10-12 18:32:21 +0300

    SQL scripts for database tables creation were prepared.

--- migrations/sqls/20181012145200-create-tables-down.sql
+++ migrations/sqls/20181012145200-create-tables-down.sql
@@ -1,3 +1,5 @@
+drop table task_tag;
+drop table task;
+drop table tag;
+drop table list;
 drop table account;
-
--- todo
--- migrations/sqls/20181012145200-create-tables-up.sql
+++ migrations/sqls/20181012145200-create-tables-up.sql
@@ -7,4 +7,38 @@ create table account (
   registration_date timestamp not null
 );
 
--- todo
+create table list (
+  id serial primary key,
+  title varchar(100) not null,
+  index integer not null,
+  account_id integer not null references account(id)
+);
+
+create table tag (
+  id serial primary key,
+  name varchar(100) not null,
+  account_id integer not null references account(id)
+);
+
+create table task (
+  id bigserial primary key,
+  title varchar(400) not null,
+  description varchar(2000) not null,
+  status varchar(20) not null,
+  parent_task_id bigint references task(id),
+  has_children boolean not null,
+  list_id integer not null references list(id),
+  index integer not null,
+  creation_date timestamp not null,
+  start_date timestamp,
+  end_date timestamp,
+  planning_date timestamp,
+  deadline timestamp,
+  check (status in ('TO_DO', 'IN_PROGRESS', 'DONE'))
+);
+
+create table task_tag (
+  task_id bigint not null references task(id),
+  tag_id integer not null references tag(id),
+  primary key(task_id, tag_id)
+);
