eaf3c53db0bc0451d08d28acb1810e25301ce8e8
Ilya Kuznetsov <et.anchoret@gmail.com>
2018-10-13 01:44:27 +0300

    JWT was integrated in the project, JWT module was implemented.

---
+++ src/util/jwt.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const appContext = require('./application-context');
+
+function sign(payload) {
+    const secret = appContext.config.jwt.secret;
+    const expiresIn = appContext.config.jwt.expiresIn;
+
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, secret, {
+            expiresIn: expiresIn
+        }, (err, token) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(token);
+            }
+        });
+    });
+}
+
+function verify(token) {
+    const secret = appContext.config.jwt.secret;
+
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(decoded);
+            }
+        })
+    });
+}
+
+module.exports = {
+    sign: sign,
+    verify: verify
+};
