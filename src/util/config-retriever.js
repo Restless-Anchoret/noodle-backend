622ad8695b2dc1d2380cda3925b47c34c504b3ca
ilya-kuznetsov <et.anchoret@gmail.com>
2018-10-12 17:24:31 +0300

    Module config-retriever was implemented, joi library was added to package.json.

---
+++ src/util/config-retriever.js
@@ -0,0 +1,68 @@
+const fs = require('fs-extra');
+const yaml = require('js-yaml');
+const joi = require('joi');
+const loggerFactory = require('./logger-factory');
+
+const log = loggerFactory.getLogger(__filename);
+
+const configSchema = joi.object({
+    db: joi.object({
+        host: joi.string().min(1).required(),
+        port: joi.number().port().required(),
+        database: joi.string().min(1).required(),
+        user: joi.string().min(1).required(),
+        password: joi.string().min(1).required(),
+        'min-connections': joi.number().integer().positive().required(),
+        'max-connections': joi.number().integer().positive().required()
+    }).required(),
+    http: joi.object({
+        port: joi.number().port().required()
+    }).required(),
+    env: joi.string().allow(['dev', 'prod', 'test'])
+});
+
+async function retrieveConfig() {
+    log.info('Reading config');
+
+    const env = getEnv();
+    log.debug('Environment:', env);
+
+    const config = await parseConfig(env);
+    log.info('Config parsing passed');
+    log.trace('Parsed config:', config);
+
+    validateConfig(config);
+    log.info('Config validation passed');
+
+    return config;
+}
+
+function getEnv() {
+    return process.env['NODE_ENV'] || 'dev';
+}
+
+async function parseConfig(env) {
+    const configPath = `config/config.${env}.yaml`;
+    log.debug('Reading config from:', configPath);
+
+    const yamlConfig = await fs.readFile(configPath, 'utf8');
+    log.trace('Read YAML config:', yamlConfig);
+
+    const config = yaml.safeLoad(yamlConfig);
+    config.env = env;
+
+    return config;
+}
+
+function validateConfig(config) {
+    const validationResult = joi.validate(config, configSchema);
+    log.trace('Validation result:', validationResult);
+
+    if (validationResult.error) {
+        throw validationResult.error;
+    }
+}
+
+module.exports = {
+    retrieve: retrieveConfig
+};
