df8a3ff248c11258a487e00c52f72353439ef293
Ilya Kuznetsov <et.anchoret@gmail.com>
2018-10-28 01:53:11 +0300

    Migrated from bcrypt library to bcryptjs library.

--- src/service/account-service.js
+++ src/service/account-service.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('../util/jwt');
 const db = require('../util/db/db');
 const accountDao = require('../dao/account-dao');
