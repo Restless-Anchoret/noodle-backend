08bb2a1e6132ea49e45f1d1d145ac7dc9af357d8
Ilya Kuznetsov <et.anchoret@gmail.com>
2018-10-28 18:38:20 +0300

    Usage of file database.json was removed from the project.

--- src/util/migration.js
+++ src/util/migration.js
@@ -9,11 +9,22 @@ const log = loggerFactory.getLogger(__filename);
 async function runMigration () {
     log.info('Database migration started');
 
-    const dbMigrateInstance = dbMigrate.getInstance(true, {
-        config: 'config/database.json',
-        env: appContext.config.env
-    });
+    const config = appContext.config;
+    const dbMigrateConfig = {
+        config: {},
+        env: config.env
+    };
 
+    dbMigrateConfig.config[config.env] = {
+        driver: 'pg',
+        user: config.db.user,
+        password: config.db.password,
+        host: config.db.host,
+        port: config.db.port,
+        database: config.db.database
+    };
+
+    const dbMigrateInstance = dbMigrate.getInstance(true, dbMigrateConfig);
     await dbMigrateInstance.up();
     log.info('Database migration finished');
 }
