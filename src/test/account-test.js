d79905f1f0ac9b722552d487237914fd0203fc75
Ilya Kuznetsov <et.anchoret@gmail.com>
2018-12-18 19:43:07 +0300

    Success case test was implemented for sign in end point.

--- src/test/account-test.js
+++ src/test/account-test.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const chai = require('chai');
+const expect = chai.expect;
+const { getRequest, postRequest, putRequest, defaultAccount } = require('./test-util');
+
 describe('Account REST API testing', () => {
     describe('Get account end point testing', () => {
         // todo
@@ -15,8 +19,16 @@ describe('Account REST API testing', () => {
 
     describe('Sign in end point testing', () => {
         it('should sign in successfully when account exists', async () => {
-            console.log('Success');
-            // todo
+            const requestBody = {
+                login: defaultAccount.login,
+                password: defaultAccount.password
+            };
+            const result = await postRequest('/account/sign-in', requestBody, null);
+
+            expect(result.body.token).to.be.a('string');
+            expect(result.body.account.id).to.be.a('number');
+            expect(result.body.account.login).to.equal(defaultAccount.login);
+            expect(result.body.account.name).to.equal(defaultAccount.name);
         });
     });
 });
--- src/test/common-test.js
+++ src/test/common-test.js
@@ -4,6 +4,8 @@ const applicationStarter = require('../util/application-starter');
 const routingConfigurator = require('../util/routing-configurator');
 const migration = require('../util/migration');
 const { env } = require('../schema/enum');
+const accountService = require('../service/account-service');
+const { defaultAccount } = require('./test-util');
 
 before(async () => {
     await applicationStarter.startApplication(env.test);
@@ -15,6 +17,13 @@ after(async () => {
 
 beforeEach(async () => {
     await migration.up();
+
+    const registerBody = {
+        login: defaultAccount.login,
+        password: defaultAccount.password,
+        name: defaultAccount.name
+    };
+    await accountService.registerAccount({ body: registerBody });
 });
 
 afterEach(async () => {
